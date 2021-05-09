04ceaa7d64c6f772802223ff96412cbe8cb2af7f
Toan73 <83698163+Toan73@users.noreply.github.com>
2021-05-09 23:07:48 +1200

    Add files via upload

---
+++ Homework Lesson_02.py
@@ -0,0 +1,35 @@
+#cau lenh input
+name = input('Enter name:')
+#name la variable
+
+print("Hello! " + name)
+
+name_1 = 'Max '
+name_2 = 'Harry '
+name_3 = 'Silver '
+last = 'nguyen '
+
+print("Chao " + name_1 + name_2 + last  + name_3 + "!")
+#nhung variable name 
+
+#example dung input va variable
+month = input('Enter month born:')
+day = input('Enter day born:')
+year = input('Enter year born:')
+print("You are born in " + month + "/" + day + "/" + year)
+
+#cung co the tinh toan voi variable
+year_born_1 = 2000
+year_born_2 = 2010
+year_born_3 = 2020
+this_year = 2021
+avg_age = ((this_year - year_born_1) + (this_year - year_born_2) + (this_year - year_born_3)) / 3
+print(avg_age)
+
+"""
+ - cac variable sai
+ 1_name, True_2, return_3, name+1, 3_name
+
+ + cac variable dung
+ year_1, year_2, April1, avg_age, Year6, animal, count1, count_9, Velocity, gravity     
+"""
